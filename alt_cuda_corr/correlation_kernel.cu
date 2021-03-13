497c56bf21254daa585141217f7ebfc0a25f818f
Vadim Kantorov <vadimkantorov@gmail.com>
2021-03-13 15:21:22 +0100

    Update correlation_kernel.cu

--- alt_cuda_corr/correlation_kernel.cu
+++ alt_cuda_corr/correlation_kernel.cu
@@ -21,7 +21,7 @@ __global__ void corr_forward_kernel(
     const torch::PackedTensorAccessor32<scalar_t,4,torch::RestrictPtrTraits> fmap2,
     const torch::PackedTensorAccessor32<scalar_t,5,torch::RestrictPtrTraits> coords,
     torch::PackedTensorAccessor32<scalar_t,5,torch::RestrictPtrTraits> corr,
-    int r)
+    int r) // radius seems measured in feature map indices (not in spatial coords - strange)
 {
   const int b = blockIdx.x; // current example index in batch
   const int h0 = blockIdx.y * blockDim.x;
